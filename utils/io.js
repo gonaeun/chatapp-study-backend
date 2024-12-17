c146cc0dc8b9eee2a17cdcfb3cf96acf29472087
고나은 <gonaeunn@gmail.com>
2024-12-18 00:11:38 +0900

    유저 로그인

--- utils/io.js
+++ utils/io.js
@@ -1,3 +1,5 @@
+const userController = require("../Controllers/user.controller")
+
 module.exports = function (io) {
     // io 관련된 모든 업무
     io.on("connection",async(socket)=>{
@@ -5,8 +7,19 @@ module.exports = function (io) {
         // 연결된 사람의 정보를 매개변수 socket에 담아둠
         console.log("client is connected, socket ID:", socket.id);
 
-    socket.on("disconnect", ()=>{
-        console.log("Client disconnected, socket ID:", socket.id);
-    });
+        socket.on("login",async(userName,cb)=>{
+                console.log("backend", userName);
+            // 유저정보를 저장
+            try{
+                const user = await userController.saveUser(userName, socket.id);
+                cb({ok:true, data:user})
+            }catch(error){
+                cb({ok:false, error:error.message});
+            }
+        });
+
+        socket.on("disconnect", ()=>{
+            console.log("Client disconnected, socket ID:", socket.id);
+        });
     })
 }
